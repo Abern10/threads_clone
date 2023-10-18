af6961a8339dff80d021201f1ddfaed473a1f910
Alex Bernatowicz <bernatowicz1122@gmail.com>
2023-10-18 12:20:27 -0500

    Test Commit

--- lib/validations/user.ts
+++
@@ -1,8 +0,0 @@
-import * as z from 'zod';
-
-export const UserValidation = z.object({
-    profile_photo: z.string().url().min(1),
-    name: z.string().min(3, {message: 'MINIMUM 3 CHARACTERS'}).max(30, {message:  'MAXIMUM 30 CHARACTERS'}),
-    username: z.string().min(3, {message: 'MINIMUM 3 CHARACTERS'}).max(30, {message:  'MAXIMUM 30 CHARACTERS'}),
-    bio: z.string().min(3, {message: 'MINIMUM 3 CHARACTERS'}).max(250, {message:  'MAXIMUM 250 CHARACTERS'})
-})
