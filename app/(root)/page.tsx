290176fc4e565846df293eca76b398a989828bb0
Alex Bernatowicz <bernatowicz1122@gmail.com>
2023-10-18 14:51:09 -0500

    test

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -23,39 +23,39 @@ async function Home({
     30
   );
 
-  // return (
-  //   <>
-  //     <h1 className='head-text text-left'>Home</h1>
+  return (
+    <>
+      <h1 className='head-text text-left'>Home</h1>
 
-  //     <section className='mt-9 flex flex-col gap-10'>
-  //       {result.posts.length === 0 ? (
-  //         <p className='no-result'>No threads found</p>
-  //       ) : (
-  //         <>
-  //           {result.posts.map((post) => (
-  //             <ThreadCard
-  //               key={post._id}
-  //               id={post._id}
-  //               currentUserId={user.id}
-  //               parentId={post.parentId}
-  //               content={post.text}
-  //               author={post.author}
-  //               community={post.community}
-  //               createdAt={post.createdAt}
-  //               comments={post.children}
-  //             />
-  //           ))}
-  //         </>
-  //       )}
-  //     </section>
+      {/* <section className='mt-9 flex flex-col gap-10'>
+        {result.posts.length === 0 ? (
+          <p className='no-result'>No threads found</p>
+        ) : (
+          <>
+            {result.posts.map((post) => (
+              <ThreadCard
+                key={post._id}
+                id={post._id}
+                currentUserId={user.id}
+                parentId={post.parentId}
+                content={post.text}
+                author={post.author}
+                community={post.community}
+                createdAt={post.createdAt}
+                comments={post.children}
+              />
+            ))}
+          </>
+        )}
+      </section> */}
 
-  //     <Pagination
-  //       path='/'
-  //       pageNumber={searchParams?.page ? +searchParams.page : 1}
-  //       isNext={result.isNext}
-  //     />
-  //   </>
-  // );
+      {/* <Pagination
+        path='/'
+        pageNumber={searchParams?.page ? +searchParams.page : 1}
+        isNext={result.isNext}
+      /> */}
+    </>
+  );
 }
 
 export default Home;
